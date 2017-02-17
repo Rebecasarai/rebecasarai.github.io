5901ba592456e3d6335d63c6f152dfc4504fae8b
rebecasarai <callmebeca@gmail.com>
2017-02-18 00:40:34 +0100

    m

--- js/quotes.js
+++ js/quotes.js
@@ -5,12 +5,37 @@
           autor: "Peter Drucker"
         },
         {
-          text: "second",
-          autor: "me"
-        }
+          text: "If you just work on stuff that you like and you’re passionate about, you don’t have to have a master plan with how things will play out.",
+          autor: "Mark Zuckerberg"
+        },
+         {
+          text: "Always deliver more than expected.",
+          autor: "Larry Page"
+        },
+         {
+          text: "You miss 100 percent of the shots you don't take.",
+          autor: "Wayne Gretzky"
+        },
+         {
+          text: "A leader is one who knows the way, goes the way and shows the way.",
+          autor: "John C. Maxwell"
+        },
+         {
+          text: "What is not started will never get finished.",
+          autor: "Goethe"
+        },
+         {
+          text: "When you cease to dream, you cease to live.",
+          autor: "Malcolm Forbes"
+        },
+        {
+          text: "Logic will get you from A to B. Imagination will take you everywhere.",
+          autor: "Albert Einstein"
+        },
+        
       ];
       var quote = quotes[Math.floor(Math.random() * quotes.length)];
       document.getElementById("quoteDisplay").innerHTML =
-        '<h1 class="quotess">' + quote.text + '</h1>'+
+        '<h1 class="quotess">“' + quote.text + '"</h1>'+
         '<h3 class="author">' + quote.autor + '</h3>';
     })();
